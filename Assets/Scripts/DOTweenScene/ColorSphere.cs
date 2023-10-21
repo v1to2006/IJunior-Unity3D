448258fd1a186c7e38dc9cc697b8c29724f8d983
v1to <pixel2006.aa@gmail.com>
2023-10-21 22:26:14 +0300

    Fix ColorSphere script

--- Assets/Scripts/DOTweenScene/ColorSphere.cs
+++ Assets/Scripts/DOTweenScene/ColorSphere.cs
@@ -12,11 +12,14 @@ public class ColorSphere : MonoBehaviour
 
     private void ChangeColor()
     {
+        Color startColor = Color.yellow;
         Color targetColor = Color.red;
         float transitionTime = 1.0f;
         int loopsCount = -1;
         LoopType loop = LoopType.Yoyo;
         Ease ease = Ease.Linear;
+
+        _material.color = startColor;
 
         _material.DOColor(targetColor, transitionTime)
             .SetLoops(loopsCount, loop)
