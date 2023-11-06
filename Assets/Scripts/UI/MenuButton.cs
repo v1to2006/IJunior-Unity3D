5057c4a25c66092d8a5567300580d4bf7d8c1213
v1to <pixel2006.aa@gmail.com>
2023-11-06 12:21:06 +0200

    Add stylized main menu

--- Assets/Scripts/UI/MenuButton.cs
+++ Assets/Scripts/UI/MenuButton.cs
@@ -5,11 +5,42 @@ public class MenuButton : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _buttonText;
 
-    private void OnSelect()
-    {
-        int additionalFontSize = 3;
+    private FontStyles _defaultfontStyle;
+    private float _defaultFontSize;
+    private Color _defaultFontColor;
 
-        _buttonText.fontStyle = FontStyles.Underline;
+    private int _pressCount = 0;
+
+	private void Awake()
+	{
+        _defaultfontStyle = _buttonText.fontStyle;
+		_defaultFontSize = _buttonText.fontSize;
+        _defaultFontColor = _buttonText.color;
+	}
+
+	public void OnPointEnter()
+    {
+        FontStyles newfontStyle = FontStyles.Underline;
+		float additionalFontSize = 3f;
+        Color newColor = Color.red;
+
+        _buttonText.fontStyle = newfontStyle;
         _buttonText.fontSize = _buttonText.fontSize + additionalFontSize;
+        _buttonText.color = newColor;
     }
+
+    public void OnPointExit()
+    {
+        _buttonText.fontStyle = _defaultfontStyle;
+        _buttonText.fontSize = _defaultFontSize;
+        _buttonText.color = _defaultFontColor;
+	}
+
+    public void OnPointClick()
+    {
+        float subtractiveFontSize = 3f;
+
+		_buttonText.fontStyle = _defaultfontStyle;
+		_buttonText.fontSize = subtractiveFontSize;
+	}
 }
